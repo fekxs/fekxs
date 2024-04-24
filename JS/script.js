faf638c922cc20e4fa418df4b83a4b4e9cd9b102
Abhin M <abhinm24@gmail.com>
2024-04-24 13:02:49 +0530

    Prototype Build

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+function header() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("main-header").innerHTML = this.responseText;
+            content("Home",document.getElementById('home_page'))   
+        }
+    };
+    xhttp.open("GET", "Common/Header.html", true);
+    xhttp.send();
+}
+
+function footer() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("main-footer").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "Common/Footer.html", true);
+    xhttp.send();
+}
+header();
+footer();
+var prev=0
+function content(page,object) {
+    if(prev==0){prev=object;object.classList.add("select")}
+    else{
+        prev.classList.remove("select");
+        object.classList.add("select")
+        prev=object
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content-body").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "Pages/"+page+".html", true);
+    xhttp.send();
+}
